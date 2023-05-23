66e86050fcb02a5eb23a4de2940473da21f981bb
Abinash <abinashmohanty227@gmail.com>
2023-05-23 14:00:02 +0530

    1st

---
+++ index.js
@@ -0,0 +1,6 @@
+let contact=document.querySelector(".contact")
+let button=document.querySelector('.request_button')
+button.addEventListener("click",()=>{
+    console.log("hello")
+    contact.classList.add('add')
+})
